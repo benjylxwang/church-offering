a9e87316aa27f0675f607dc4c6ca218617635cc2
Benjamin Wang <benjylxwang@hotmail.com>
2024-03-03 18:40:05 +0000

    fix: remove header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import OfferingForm from "./components/forms/OfferingForm";
 import { Container, Stack } from "@mui/material";
-import Header from "./components/Header";
 
 function App() {
   return (
     <Container>
       <Stack gap={4} mb={4}>
-        <Header />
         <OfferingForm />
       </Stack>
     </Container>
